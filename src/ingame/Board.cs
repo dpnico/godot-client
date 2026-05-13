d496b70a7989f831b7932e91a3aa8a744dc9c97e
yot96 <nicolina-1.liedl@uni-ulm.de>
2026-05-14 01:06:56 +0200

    feat: Main menu scene setup

--- src/ingame/Board.cs
+++ src/ingame/Board.cs
@@ -10,12 +10,17 @@ public partial class Board : Node2D
 	[Export] private TileMapLayer _ships;
 	[Export] private TileMapLayer _markers;
 	
-	// Called when the node enters the scene tree for the first time.
+	/// <summary>
+	/// Called when the node enters the scene tree for the first time.
+	/// </summary>
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	/// <summary>
+	/// Called every frame. 'delta' is the elapsed time since the previous frame.
+	/// </summary>
+	/// <param name="delta"></param>
 	public override void _Process(double delta)
 	{
 	}
---
+++ src/main_menu/MainMenu.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+/// <summary>
+/// The main menu with Start, Settings, Help and Exit button. It is the first
+/// scene that gets loaded when the game is started.
+/// </summary>
+public partial class MainMenu : Control
+{
+	[Export] private Button _startButton;
+	[Export] private Button _settingsButton;
+	[Export] private Button _helpButton;
+	[Export] private Button _exitButton;
+	
+	/// <summary>
+	/// Called when the node enters the scene tree for the first time.
+	/// </summary>
+	public override void _Ready()
+	{
+		// _startButton.Pressed += ...
+		// _settingsButton.Pressed += ...
+		// _helpButton.Pressed += 
+		_exitButton.Pressed += () => GetTree().Quit();
+	}
+
+	/// <summary>
+	/// Called every frame. 'delta' is the elapsed time since the previous frame.
+	/// </summary>
+	/// <param name="delta"></param>
+	public override void _Process(double delta)
+	{
+	}
+}
