ad3f5c1820e5edca722a5d0e0da8805ebbd77b3c
yot96 <nicolina-1.liedl@uni-ulm.de>
2026-05-13 23:43:11 +0200

    feat: Board scene setup

---
+++ src/ingame/Board.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Represents a singlular player's board state and handles its logic.
+/// </summary>
+public partial class Board : Node2D
+{
+	[Export] private TileMapLayer _grid;
+	[Export] private TileMapLayer _markers;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
