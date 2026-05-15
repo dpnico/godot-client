e80abe7c995b5a01127940b6c1712cf76376e6e6
yot96 <nicolina-1.liedl@uni-ulm.de>
2026-05-15 19:06:50 +0200

    test: Fix assertion

--- test/pop_up/PopUpDataTests.cs
+++ test/pop_up/PopUpDataTests.cs
@@ -12,11 +12,11 @@ public class PopUpDataTests
     [TestCase]
     public void PopUpSceneMapTest()
     {
-        var singleButtonPopUpScene = GD.Load<PackedScene>("res://scenes/pop_up/single_button_pop_up.tscn");
-        var dualButtonPopUpScene = GD.Load<PackedScene>("res://scenes/pop_up/dual_button_pop_up.tscn");
+        var singleButtonPopUpPath = "res://scenes/pop_up/single_button_pop_up.tscn";
+        var dualButtonPopUpPath = "res://scenes/pop_up/dual_button_pop_up.tscn";
 
         var exitGame = PopUpMap.Scene[PopUpType.EXIT_GAME];
 
-        AssertThat(exitGame).IsEquivalentTo(dualButtonPopUpScene);
+        AssertThat(exitGame.ResourcePath).IsEqual(dualButtonPopUpPath);
     }
 }
