15039d67daea2e6b6b84ddd113a623d9031a4331
yot96 <nicolina-1.liedl@uni-ulm.de>
2026-05-16 15:10:38 +0200

    fix: Remove pop-ups from scene tree

--- src/pop_up/PopUpManager.cs
+++ src/pop_up/PopUpManager.cs
@@ -66,6 +66,7 @@ public class PopUpManager
         var popUp = _popUpStack.Pop();
         ShowMostRecent(true);
         PopUpRemoved?.Invoke(popUp.GetPopUpType());
+        popUp.QueueFree();
     }
 
     /// <summary>
