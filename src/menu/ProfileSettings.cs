4b631dcd414b8dd8e89babaeee776b0ee6eaa905
yot96 <nicolina-1.liedl@uni-ulm.de>
2026-05-24 22:39:30 +0200

    feat: Clicking anything but Save discards entered username

--- src/menu/ProfileSettings.cs
+++ src/menu/ProfileSettings.cs
@@ -11,7 +11,7 @@ public partial class ProfileSettings : PanelContainer
     [Export] private Label _username;
     [Export] private LineEdit _usernameEdit;
     [Export] private Button _usernameEditButton;
-
+    
     private bool _editingUsername = false;
 
     /// <summary>
@@ -34,7 +34,19 @@ public partial class ProfileSettings : PanelContainer
 
     public override void _Input(InputEvent @event)
     {
-
+        if (@event is InputEventMouseButton mb && mb.Pressed) 
+        {
+            var pos = GetGlobalMousePosition();
+            var hoveredControl = GetViewport().GuiGetHoveredControl();
+            if (_editingUsername)
+            {
+                if (hoveredControl != _usernameEditButton && hoveredControl != _usernameEdit)
+                {
+                    GD.Print("Editing terminated early.");
+                    TerminateEditingUsername();
+                }
+            }
+        }
     }
 
     public void OnUsernameEditButtonPressed()
@@ -42,9 +54,7 @@ public partial class ProfileSettings : PanelContainer
         if (_editingUsername)
         {
             TrySaveUsername();
-            ActivateControl(_usernameEdit, false);
-            _usernameEditButton.Text = "Edit";
-            _editingUsername = false;
+            TerminateEditingUsername();
         }
         else
         {
@@ -69,8 +79,15 @@ public partial class ProfileSettings : PanelContainer
                 PopUpManager.Instance.ShowPopUp(PopUpType.USERNAME_TOO_LONG);
             }
         }
+    }
+    
+    public void TerminateEditingUsername() 
+    {
+        ActivateControl(_usernameEdit, false);
         _usernameEdit.Text = "";
         _usernameEdit.PlaceholderText = _username.Text;
+        _usernameEditButton.Text = "Edit";
+        _editingUsername = false;
     }
 
     public void ActivateControl(Control ctrl, bool active)
